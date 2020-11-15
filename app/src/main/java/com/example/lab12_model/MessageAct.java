0aa4790f8b6adcdb653315285b20ac504cc1acde
Thisuraka <64680083+Thisuraka@users.noreply.github.com>
2020-11-15 23:49:48 +0530

    stage 2

--- app/src/main/java/com/example/lab12_model/MessageAct.java
+++ app/src/main/java/com/example/lab12_model/MessageAct.java
@@ -3,12 +3,26 @@ package com.example.lab12_model;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MessageAct extends AppCompatActivity {
+    TextView msgTit;
+    EditText teaMesEt2;
+    String Subject, Message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messageact);
+
+        msgTit = findViewById(R.id.msgTit);
+        teaMesEt2 = findViewById(R.id.teaMesEt2);
+
+        Subject = getIntent().getStringExtra("Sub");
+        Message = getIntent().getStringExtra("Msg");
+
+        msgTit.setText(Subject);
+        teaMesEt2.setText(Message);
     }
 }
--- app/src/main/java/com/example/lab12_model/Student.java
+++ app/src/main/java/com/example/lab12_model/Student.java
@@ -1,10 +1,15 @@
 package com.example.lab12_model;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -15,14 +20,14 @@ import java.util.List;
 public class Student extends AppCompatActivity {
 
     dbHandler dbHandler;
-    ArrayList<Message> messages;
-    List<String> finalList = new ArrayList<>();
+    ArrayList<Message> messages; //where the values from DB would be saved
+    List<String> finalList = new ArrayList<>(); //additional list to cast to listview
     Context context;
     TextView stdTit;
     ListView stdLv;
 
-    String user, subject, message;
-    Integer pointer = 0, size;
+    String user, subject, message; //user is for setting the text
+    Integer pointer = 0, size; //related to finalList
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,37 +38,68 @@ public class Student extends AppCompatActivity {
         stdTit = findViewById(R.id.stdTit);
         stdLv = findViewById(R.id.stdLv);
 
+        //========================setting textView to user name ====================
         user = getIntent().getStringExtra("Student");
         stdTit.setText("Welcome " + user);
 
+        //======================== Getting the values from DB ====================
         messages = new ArrayList<>();
-
         messages = (ArrayList<Message>) dbHandler.getAllMessages();
+        size = messages.size();
 
         Log.i("Tag", "this shit" + messages);
 
-        size = messages.size();
-
+        //======================== extracting data ========================
         while (size != 0) {
             Message test = messages.get(pointer);
 
             subject = test.getSubject();
             message = test.getMessage();
 
-            finalList.add("Subject : " + subject + "\n" +"Message: " + message);
+            finalList.add(subject); //"Subject : " + subject + "\n" + "  " + message
 
             pointer++;
             size--;
         }
 
+        //======================== setting to the listView ========================
         final ArrayAdapter<String> adapter = new ArrayAdapter<>
                 (this, android.R.layout.simple_list_item_1, finalList);
         stdLv.setAdapter(adapter);
 
-//        MessageAdapter adapter = new MessageAdapter(context,R.layout.activity_single_message,messages);
-//        stdLv.setAdapter(adapter);
+        //======================== When item is clicked ========================
+        stdLv.setOnItemClickListener((adapterView, view, position, id) -> {
 
+            String value = stdLv.getAdapter().getItem(position).toString(); //for title
+            StringBuffer buffer = new StringBuffer();
+            buffer.append(adapterView.getItemAtPosition(position) + "\n");
 
+            Message test = messages.get(position); //again took coz why not . sue me
+
+            subject = test.getSubject();
+            message = test.getMessage();
+
+            ShowMessage(value, message); //calls the little dialog box function
+
+            //======== Optional In case message activity needs to be invoked =========
+            // ======= But why bother after i made so much effort on the adapter ====
+               /* Intent intent = new Intent(getBaseContext(), MessageAct.class);
+                intent.putExtra("Sub", subject);
+                intent.putExtra("Msg", message);
+                startActivity(intent);*/
+        });
+
+    }
+
+    //======================== No clue what happens here but it works========================
+    //======================== Don't you fuken mess with it ========================
+    public void ShowMessage(String subject, String message) {
+        Activity activity = this;
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setCancelable(true);
+        builder.setTitle(subject);
+        builder.setMessage(message);
+        builder.show();
     }
 }
 
